d7729d21118000417386063a3b5840b2e6bfa5b7
Bobloy <alboblexloy@gmail.com>
2017-12-28 11:18:46 -0500

    Enable waitlist

--- legend/legend.py
+++ legend/legend.py
@@ -955,9 +955,9 @@ class legend:
         author = ctx.message.author
         legendServer = ["374596069989810176"]
 
-        if server.id not in legendServer:
-            await self.bot.say("This command can only be executed in the LeGeND Family Server")
-            return
+        # if server.id not in legendServer:
+            # await self.bot.say("This command can only be executed in the LeGeND Family Server")
+            # return
 
         await self.bot.type()
 
